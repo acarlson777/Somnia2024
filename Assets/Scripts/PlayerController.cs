c867dbe41ac02c9e221a165eea7a08df450f65f8
Andrew <andrewchung669@gmail.com>
2023-09-22 09:53:06 -0700

    added toysrus
    added iso scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] Transform transform;
+    [SerializeField] Rigidbody rb;
+    private Vector3 input;
+    private float speed = 5f;
+    Vector3 movement;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void PlayerInput()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void Move()
+    {
+        movement = input.normalized;
+        movement = Quaternion.Euler(0, 45, 0) * movement;
+        movement = movement.normalized;
+
+        if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            if (Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+            {
+                transform.Translate(movement * 9 * Time.deltaTime);
+            }
+        }
+        else
+        {
+            transform.Translate(movement * speed * Time.deltaTime);
+        }
+    }
+
+}
