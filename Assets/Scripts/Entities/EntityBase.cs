145e06aba1d668558cc85265b6f7cea8604c8deb
Leonardo Garcia Sanchez <lgarciasanchez5450@gmail.com>
2023-09-29 10:36:30 -0700

    Worked on Entity Framework and added Brain

---
+++ Assets/Scripts/Entities/EntityBase.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Each entity should be a:
+    //    - World Collidable(aka. collider
+    //    - Interactable via Interact button
+    //    - Questions: how big should the interact collider be????
+    private Rigidbody rb;
+    public BoxCollider entityCollider;
+    public Collider interactCollider;
+    public float BoxColliderThickness;
+
+    private Vector3 accel;
+    private Vector3 vel;
+    private float entityMaxSpeed = 4f;
+    protected void Initialize()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+
+    public void Start()
+    {
+        Initialize();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (vel.magnitude > entityMaxSpeed)
+        {
+            vel *= entityMaxSpeed / vel.magnitude; // Set the magnitude to maxSpeed
+        }
+        rb.velocity = vel;
+    }
+    void Move(Vector3 accel)
+    {
+        vel += accel;
+    }
+    protected void PrintName()
+    {
+        print(GetType().Name);
+    }
+}
