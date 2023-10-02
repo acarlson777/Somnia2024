8f63a1107bdd0c3faa96bfe6e7f9ccb11394ceeb
Leonardo Garcia Sanchez <lgarciasanchez5450@gmail.com>
2023-10-02 09:42:50 -0700

    Entity System Doesn't Break things

--- Assets/Scripts/Entities/LivingScripts/Brains/Brain.cs
+++ Assets/Scripts/Entities/LivingScripts/Brains/Brain.cs
@@ -5,5 +5,15 @@ public class Brain
 {
     // The Entities should be responsible for sending the data to the brain.
     public ArrayList EntitiesSeen;
-    public ArrayList 
+    //Should return a (x,y) within a unit circle which can be used by the Living Entities to accelerate
+    public Vector2 GetDirectionVelocity()
+    {
+        return Vector2.zero;
+    }
+    // Should be called every time another entity wants to talk to this ones. Should be passed through entity
+    public Vector2 Talk(EntityBase entity)
+    {
+        return Vector2.zero;
+    }
 }
+    
--- Assets/Scripts/Entities/NonLivingScripts/NonLiving.cs
+++ Assets/Scripts/Entities/NonLivingScripts/NonLiving.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NonLiving : Entities
+public class NonLiving : EntityBase
 {
     // Start is called before the first frame update
     void Start()
