3447a8dc51fa4c139f8201a04c23ed9134b34c4e
Andrew <andrewchung669@gmail.com>
2023-10-13 10:36:15 -0700

    wip more dialogeh stuff

--- Assets/Scripts/Entities/LivingScripts/TESTNPC/TESTNPC.cs
+++ Assets/Scripts/Entities/LivingScripts/TESTNPC/TESTNPC.cs
@@ -6,6 +6,9 @@ public class TESTNPC : Living,Entity
 {
 
     SetDialogueBoxText dialogue;
+
+    public string[] futureDialogue;
+    int dialogueLine = 0;
     // Start is called before the first frame update
     new void Start()
     {
@@ -20,7 +23,22 @@ public class TESTNPC : Living,Entity
     }
     new public void Interact(Entity entity) // Interact is a method which can be overridden but does not need to call the base version
     {
-        print("interacting");
-        dialogue.Talk();
+        //dialogue.lines[0] = futureDialogue[dialogueLine];
+        if (dialogue.lines.Length > 0)
+        {
+            print("interacting");
+            dialogue.Talk();
+            if (futureDialogue.Length == 0)
+            {
+                return;
+            }
+            if (futureDialogue.Length > 0)
+            {
+                dialogue.lines = new string[futureDialogue.Length];
+            }
+        }
+        
+
+        //dialogueLine++;
     }
 }
