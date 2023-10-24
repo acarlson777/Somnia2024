6884dd91e9ba1a4c8aa87f3ce851d61d8fcfda58
Andrew <andrewchung669@gmail.com>
2023-10-24 10:37:16 -0700

    more menu work + added art stuff

--- Assets/Scripts/Entities/LivingScripts/TESTNPC/TESTNPC.cs
+++ Assets/Scripts/Entities/LivingScripts/TESTNPC/TESTNPC.cs
@@ -4,98 +4,32 @@ using UnityEngine;
 
 public class TESTNPC : NPCs,Entity
 {
-
-    public List<VoiceLines> tee;
-
+    public List<VoiceLines> dialogueList;
     public int timesInteracted = 0;
 
-
-    // Start is called before the first frame update
     new void Start()
     {
         base.Start();
-
     }
 
-    // Update is called once per frame
     new void Update()
     {
         base.Update();
     }
+
+    // on interaction, open the dialogue box and start writing text that is set in the inspector
     new public void Interact(Entity entity)
     {
-        if (timesInteracted > tee.Count-1)
+        print(dialogueList.Count);
+
+        if (timesInteracted >= dialogueList.Count)
         {
-            timesInteracted = tee.Count-1;
+            timesInteracted = dialogueList.Count-1;
         }
-        DialogueManager.PopDialogue(tee[timesInteracted].lines);
+        DialogueManager.PopDialogue(dialogueList[timesInteracted].lines);
         timesInteracted++;
     }
-
-    /*public void _Interact(Entity entity) // Interact is a method which can be overridden but does not need to call the base version
-    {
-        
-        // Keep same for every npc / entity
-        if (timesInteracted == numberOfFutureDialogue)
-        {
-            dialogue.Talk();
-        }
-        if (timesInteracted == 0)
-         {
-             print("interacting");
-             dialogue.Talk();
-            if (futureDialogueExists)
-            {
-                timesInteracted++;
-            }
-             return;
-         }
-
-        // change for every npc / entity
-         if (timesInteracted == 1)
-         {
-            string [] futureDialogue = { "Hello", "whats up"};
-            SetFutureDialogue(2, futureDialogue);
-         }
-         if (timesInteracted == 2)
-         {
-            string[] futureDialogue = { "How inefficient is this", "slightly better than last time", "yay!" };
-            SetFutureDialogue(3, futureDialogue);
-         }
-
-         
-
-        void SetFutureDialogue(int length, string[] dialogueLines)
-        {
-             dialogue.lines = new string[length];
-             for (int i = 0; i < length; i++)
-             {
-                 dialogue.lines[i] = dialogueLines[i];
-             }
-             dialogue.Talk();
-             timesInteracted++;
-             return;
-
-        }
-        /*
-        if (hasInteracted == false)
-        {
-            print("interacting");
-            dialogue.Talk();
-            if (futureDialogue.Length > 0)
-            {
-                hasInteracted = true;
-            }
-        }
-        if (hasInteracted == true)
-        {
-            dialogue.lines = futureDialogue;
-            dialogue.Talk();
-        }
-        
-
-    }
-    */
+    
     [System.Serializable]
     public class VoiceLines
     {
---
+++ Assets/Scripts/Mechanics/SceneLoading/TapToStart.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapToStart : MonoBehaviour
+{
+
+    InstantiateLoadingScreen loadingScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadingScreen = GetComponent<InstantiateLoadingScreen>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            loadingScreen.LoadANewScene();
+        }
+    }
+}
