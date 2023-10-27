4a8f8eb8742d08fcd1ba075256204b7e1d0789ea
WizardPenguin77 <andrewchung669@gmail.com>
2023-10-26 21:21:58 -0700

    main menu thing done
    also for some reason the interact button script got disconnected when i pulled to my pc i think i put it back right but im honestly not sure is it supposed to debuglog something

--- Assets/Scripts/Mechanics/MainMenu/FadeText.cs
+++ Assets/Scripts/Mechanics/MainMenu/FadeText.cs
@@ -5,10 +5,15 @@ using TMPro;
 
 public class FadeText : MonoBehaviour
 {
+
+    public TextMeshProUGUI startText;
+    public float fadeSpeed = 1f;
+    float waitSpeed = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(FadeTextInNOut());
     }
 
     // Update is called once per frame
@@ -16,4 +21,31 @@ public class FadeText : MonoBehaviour
     {
         
     }
+
+    IEnumerator FadeTextInNOut()
+    {
+        while (true)
+        {
+            // Fade in the text
+            for (float i = 0; i < fadeSpeed; i += Time.deltaTime)
+            {
+                float opacity = Mathf.Lerp(0f, 1f, i / fadeSpeed);
+                startText.color = new Color(startText.color.r, startText.color.g, startText.color.b, opacity);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(1.0f);
+
+            // Fade out the text
+            for (float i = 0; i < fadeSpeed; i += Time.deltaTime)
+            {
+                float opacity = Mathf.Lerp(1f, 0f, i / fadeSpeed);
+                startText.color = new Color(startText.color.r, startText.color.g, startText.color.b, opacity);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(waitSpeed);
+        }
+    }
+
 }
