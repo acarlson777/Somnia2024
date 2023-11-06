4d1770c21383afa97e10990103c922c3960c6373
Leonardo Garcia Sanchez <lgarciasanchez5450@gmail.com>
2023-11-06 10:35:45 -0800

    Finished Gate, and Password Symbol Started

--- Assets/Scripts/Mechanics/Scenes/Maze/MazeGate.cs
+++ Assets/Scripts/Mechanics/Scenes/Maze/MazeGate.cs
@@ -3,20 +3,40 @@ using System.Collections;
 
 public class MazeGate : InteractableObject, Entity
 {
+    private Vector3 startPos;
+    public float gateSpeed = 1.0f;
+    public float gateEndDistance = 3.0f;
+    private Vector3 endPos;
 
+    [SerializeField]private bool open = false;
 
     public new void Start()
     {
         base.Start();
+        startPos = transform.position;
+        endPos = new Vector3(startPos.x, startPos.y + gateEndDistance, startPos.z);
     }
 
     public new void Update()
     {
         base.Update();
+        if (open)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPos, Time.deltaTime * gateSpeed);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPos, Time.deltaTime * gateSpeed);
+        }
     }
 
     public new void Interact(Entity entity)
     {
 
+    }
+
+    public void OpenGate()
+    {
+        open = true;
     }
 }
--- Assets/Scripts/Mechanics/Scenes/Maze/PasswordSymbol.cs
+++ Assets/Scripts/Mechanics/Scenes/Maze/PasswordSymbol.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PasswordSymbol : InteractableObject, Entity
 {
 
-
+    [SerializeField] private bool lit = false;
     public new void Start()
     {
         base.Start();
@@ -14,9 +14,15 @@ public class PasswordSymbol : InteractableObject, Entity
     {
         base.Update();
     }
-
+    public bool getLit()
+    {
+        return lit;
+    }
     public new void Interact(Entity entity)
     {
-
-    }
+        // if the player has interacted with us then just light on or off!
+        lit = !lit;
+        Material onMat = GameObject.Find("Gate").GetComponent<Renderer>().material;
+        gameObject.GetComponent<Renderer>().material = onMat;
+    }       
 }
