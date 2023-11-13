94bcdd25ec8693b67796a7039b0f236bbbfd0455
Carlson <acarlson777@monroviaschools.net>
2023-11-13 10:35:42 -0800

    Settings Button

---
+++ Assets/Scripts/Mechanics/EternallyActive/SettingsButton/SettingsButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsButtonScript : MonoBehaviour
+{
+    private bool isOn = true;
+    private GameObject settingsScreen;
+
+
+    void Start()
+    {
+        settingsScreen = GameObject.Find("SettingsButtonScreen");
+        Toggle();
+        //Use settingsScreen gameObject to get the sliders and any other things (might not be necessary because the volume sliders handle themselves
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+
+    public void Toggle()
+    {
+        isOn = !isOn;
+        settingsScreen.SetActive(isOn);
+    }
+}
