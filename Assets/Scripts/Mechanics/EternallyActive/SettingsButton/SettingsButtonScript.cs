872d5d01ea71c71c34c8f48cb19317e734502cd5
Carlson <acarlson777@monroviaschools.net>
2023-11-14 09:45:39 -0800

    Settings Button Remove Joystick and Remove Interactable Button

--- Assets/Scripts/Mechanics/EternallyActive/SettingsButton/SettingsButtonScript.cs
+++ Assets/Scripts/Mechanics/EternallyActive/SettingsButton/SettingsButtonScript.cs
@@ -5,11 +5,15 @@ public class SettingsButtonScript : MonoBehaviour
 {
     private bool isOn = true;
     private GameObject settingsScreen;
+    private GameObject joystick;
+    private GameObject interactableButton;
 
 
     void Start()
     {
         settingsScreen = GameObject.Find("SettingsButtonScreen");
+        joystick = GameObject.Find("Joystick");
+        interactableButton = GameObject.Find("InteractableButton");
         Toggle();
         //Use settingsScreen gameObject to get the sliders and any other things (might not be necessary because the volume sliders handle themselves
     }
@@ -25,5 +29,7 @@ public class SettingsButtonScript : MonoBehaviour
     {
         isOn = !isOn;
         settingsScreen.SetActive(isOn);
+        joystick.SetActive(!isOn);
+        interactableButton.SetActive(!isOn);
     }
 }
