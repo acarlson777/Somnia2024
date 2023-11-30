2a249a88b7518e0c11d9041b454b0508286a6f64
Carlson <acarlson777@monroviaschools.net>
2023-11-30 10:35:47 -0800

    Working on LightHandling For Maze

--- Assets/Scripts/Mechanics/Scenes/Maze/GateSymbol.cs
+++ Assets/Scripts/Mechanics/Scenes/Maze/GateSymbol.cs
@@ -6,10 +6,15 @@ public class GateSymbol : InteractableObject, Entity
 {
     public string[] password; // this will hold an array of string which must be true and nothing else must be true for this symbol to be turned on.
     public bool glowy = false;
+    public Material startingMat;
+    public Material onMat;
+    public List<Voicelines> lines;
+    private int timesInteracted = 0;
 
     public new void Start()
     {
         base.Start();
+        gameObject.GetComponent<Renderer>().material = startingMat;
     }
 
     public new void Update()
@@ -17,10 +22,26 @@ public class GateSymbol : InteractableObject, Entity
         base.Update();
     }
 
-    public new void Interact(Entity entity)
+    new public void Interact(Entity entity)
     {
+        if (entity is Player)
+        {
+            if (timesInteracted >= lines.Count)
+            {
+                timesInteracted = lines.Count - 1;
+            }
+            print("putting a dialogue" + timesInteracted);
+            DialogueManager.PopDialogue(lines[timesInteracted].lines);
+            timesInteracted++;
 
+        }
     }
+    [System.Serializable]
+    public class Voicelines
+    {
+        public string[] lines;
+    }
+
     public bool submitPassword(string[] passwordSubmitted)
     {
         if (password.Length != passwordSubmitted.Length) return false;
@@ -30,6 +51,7 @@ public class GateSymbol : InteractableObject, Entity
         if (actualPassword.SetEquals(userPassword))
         {
             glowy = true;
+            gameObject.GetComponent<Renderer>().material = onMat;
             return true;
         }
         else return false;
