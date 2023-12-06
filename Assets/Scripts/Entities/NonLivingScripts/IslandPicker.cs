29501c4a650b422b3522c6786fa1673911c32be9
Leonardo Garcia Sanchez <lgarciasanchez5450@gmail.com>
2023-12-06 10:01:43 -0800

    Island Picker Works

---
+++ Assets/Scripts/Entities/NonLivingScripts/IslandPicker.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandPicker : NonLiving, Entity
+{
+
+    InstantiateLoadingScreen loadingScreen;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        loadingScreen = GetComponent<InstantiateLoadingScreen>();
+    }
+    new public void Interact(Entity entity)
+    {
+        if (entity is Player)
+        {
+            loadingScreen.LoadANewScene();
+        }
+    }
+}
