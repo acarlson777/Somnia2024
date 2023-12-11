f5b629c31b983fde37afe35f25139ec9cea089bd
Leonardo Garcia Sanchez <lgarciasanchez5450@gmail.com>
2023-12-11 10:21:21 -0800

    infff

--- Assets/Scripts/Mechanics/Scenes/Maze/InfiniteCorridorHandler.cs
+++ Assets/Scripts/Mechanics/Scenes/Maze/InfiniteCorridorHandler.cs
@@ -17,14 +17,17 @@ public class InfiniteCorridorHandler : MonoBehaviour
         {
             madeNextArea = true;
             Vector3 newTransformRight = (new Vector3(distanceOfExpansion,0,0)+transform.localPosition);
-            newTransformRight.y = transform.position.y;
+            newTransformRight.y = transform.localPosition.y;
+            
             print("Next Transform should be " + newTransformRight);
 
             GameObject next =  Instantiate(gameObject);
+            next.transform.parent = gameObject.transform.parent;
+            next.transform.localRotation = gameObject.transform.localRotation;
             next.transform.localPosition = newTransformRight;
             
             InfiniteCorridorHandler handler = next.GetComponent<InfiniteCorridorHandler>();
-            //handler.parent = parent
+            handler.parent = parent ;;
         }
     }
 }
