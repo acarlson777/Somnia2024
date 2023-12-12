28056958a7a2692e654319513b2c2d8ce3c6d5e8
WizardPenguin77 <andrewchung669@gmail.com>
2023-12-11 22:00:40 -0800

    UH DO NOT PULL ANYTHING
    This reverts commit fb776eb79a663b15938fa0d65ce93d49c467e8f9, reversing
    changes made to 8e03cd919a6752ae147e7f1d08c1aea45ddacce7.

--- Assets/Scripts/Mechanics/Scenes/Maze/InfiniteCorridorHandler.cs
+++ Assets/Scripts/Mechanics/Scenes/Maze/InfiniteCorridorHandler.cs
@@ -17,17 +17,14 @@ public class InfiniteCorridorHandler : MonoBehaviour
         {
             madeNextArea = true;
             Vector3 newTransformRight = (new Vector3(distanceOfExpansion,0,0)+transform.localPosition);
-            newTransformRight.y = transform.localPosition.y;
-            
+            newTransformRight.y = transform.position.y;
             print("Next Transform should be " + newTransformRight);
 
             GameObject next =  Instantiate(gameObject);
-            next.transform.parent = gameObject.transform.parent;
-            next.transform.localRotation = gameObject.transform.localRotation;
             next.transform.localPosition = newTransformRight;
             
             InfiniteCorridorHandler handler = next.GetComponent<InfiniteCorridorHandler>();
-            handler.parent = parent ;;
+            //handler.parent = parent
         }
     }
 }
