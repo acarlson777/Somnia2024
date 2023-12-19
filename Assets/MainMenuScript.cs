cdfec5e2d302b92dca9a52e938462e5ff6958213
Carlson <acarlson777@monroviaschools.net>
2023-12-19 11:25:07 -0800

    asd

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    InstantiateLoadingScreen sceneLoader;
+    public string soundtrackToStop;
+    public void Start()
+    {
+        sceneLoader = GetComponent<InstantiateLoadingScreen>();
+    }
+
+    public void GoToMainMenu()
+    {
+        AudioManagerSingleton.Instance.FadeOutAndStopSoundtrack(soundtrackToStop, 1f);
+        sceneLoader.LoadANewScene();
+    }
+}
