ee5a7ec286b9093f038c54747ff11f354a11670f
WizardPenguin77 <andrewchung669@gmail.com>
2024-01-09 17:52:04 -0800

    menu plays we ball

--- Assets/Scripts/Mechanics/Scenes/MainMenu/FadeText.cs
+++ Assets/Scripts/Mechanics/Scenes/MainMenu/FadeText.cs
@@ -8,18 +8,42 @@ public class FadeText : MonoBehaviour
 
     public TextMeshProUGUI startText;
     public float fadeSpeed = 1f;
-    float waitSpeed = 0.5f;
+    float waitSpeed = 12.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(FadeTextInNOut());
+        StartCoroutine(FadeTextIn());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator FadeTextIn()
+    {
+        while (true)
+        {
+            startText.color = new Color(startText.color.r, startText.color.g, startText.color.b, 0);
+            yield return new WaitForSeconds(waitSpeed);
+            for (float i = 0; i < fadeSpeed; i += Time.deltaTime)
+            {
+                float opacity = Mathf.Lerp(0f, 1f, i / fadeSpeed);
+                startText.color = new Color(startText.color.r, startText.color.g, startText.color.b, opacity);
+                yield return null;
+            }
+            yield return new WaitForSeconds(64f);
+
+            for (float i = 0; i < fadeSpeed; i += Time.deltaTime)
+            {
+                float opacity = Mathf.Lerp(1f, 0f, i / fadeSpeed);
+                startText.color = new Color(startText.color.r, startText.color.g, startText.color.b, opacity);
+                yield return null;
+            }
+            yield return new WaitForSeconds(waitSpeed);
+        }
     }
 
     IEnumerator FadeTextInNOut()
