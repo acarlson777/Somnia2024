c3eb1ae9d17ad445ab2a6b0f52be6e89e1925773
WizardPenguin77 <andrewchung669@gmail.com>
2024-01-14 00:08:17 -0800

    least stupid me moment

--- Assets/Scripts/Mechanics/EternallyActive/AudioManager/VolumeSlider.cs
+++ Assets/Scripts/Mechanics/EternallyActive/AudioManager/VolumeSlider.cs
@@ -7,51 +7,33 @@ public class VolumeSlider : MonoBehaviour
 
     public string type;
     private Slider slider;
-    private bool defaultRun;
-    private int sliderCount;
 
     void Start()
     {
         slider = GetComponent<Slider>();
-        defaultRun = PlayerPrefs.GetInt("DefaultRun", 0) == 0;
-        slider.value = 0;
-
-        if (defaultRun)
+        if (type == "sfx")
         {
-            slider.value = 1;
-            PlayerPrefs.SetInt("DefaultRun", 0);
+            slider.value = PlayerPrefs.GetFloat("sfxVolume",1);
         }
-
+        else if (type == "song")
+        {
+            slider.value = PlayerPrefs.GetFloat("songVolume",1);
+        }
         else
         {
-
-            if (type == "sfx")
-            {
-                slider.value = PlayerPrefs.GetFloat("sfxVolume");
-            }
-            else if (type == "song")
-            {
-                slider.value = PlayerPrefs.GetFloat("songVolume");
-            }
-            else
-            {
-                Debug.Log("Invalid Sound Type");
-            }
+            Debug.Log("Invalid Sound Type");
         }
     }
 
     void Update()
     {
-        if (!defaultRun)
+        if (type == "sfx")
         {
-            if (type == "sfx")
-            {
-                PlayerPrefs.SetFloat("sfxVolume", slider.value);
-            }
-            else if (type == "song")
-            {
-                PlayerPrefs.SetFloat("songVolume", slider.value);
-            }
+            PlayerPrefs.SetFloat("sfxVolume", slider.value);
+        }
+        else if (type == "song")
+        {
+            PlayerPrefs.SetFloat("songVolume", slider.value);
         }
     }
 }
