5d8df8340dbc87e6d83ca1bd4d22cbf48bcbc54a
Unknown <arturszczurowski95@gmail.com>
2024-03-08 09:48:46 -0800

    Added memory leaks

--- Assets/Scripts/Mechanics/Scenes/Dungeon & Kitchen/DungeonManager.cs
+++ Assets/Scripts/Mechanics/Scenes/Dungeon & Kitchen/DungeonManager.cs
@@ -4,32 +4,25 @@ using UnityEngine;
 
 public class DungeonManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     [SerializeField]
-    public DungeonBlock block1, block2, block3;
-    public DungeonPressurePlate plate1, plate2, plate3;
+    public PushBlock block1, block2, block3;
+    public PressurePlate plate1, plate2, plate3;
     public DungeonGate gate;
-
 
     // Update is called once per frame
     void Update()
     {
 
       // Check if all blocks are on their pressure plates
-      if (plate1.isOccupied && plate2.isOccupied && plate3.isOccupied)
-      {
+      if (plate1.isOccupied && plate2.isOccupied && plate3.isOccupied) {
           // Open the gate if all conditions are met
           gate.OpenGate();
-      }
-      else
-      {
+
+      } else {
           // Close the gate if any block is not on its pressure plate
           gate.CloseGate();
+
       }
 
     }
