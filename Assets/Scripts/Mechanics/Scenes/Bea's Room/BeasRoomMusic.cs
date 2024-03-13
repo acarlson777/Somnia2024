8ecba3208c05c4e6e80e4cfc5d9aa22d15020253
Daniel <dmoran55555@gmail.com>
2024-03-13 10:06:40 -0700

    sadf

--- Assets/Scripts/Mechanics/Scenes/Bea's Room/BeasRoomMusic.cs
+++ Assets/Scripts/Mechanics/Scenes/Bea's Room/BeasRoomMusic.cs
@@ -13,5 +13,12 @@ public class BeasRoomMusic : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
         AudioManagerSingleton.Instance.FadeInRandomSongFromSoundtrack("BeasRoom", 1f, gameObject);
+        while (true) {
+        yield return new WaitForSeconds(Random.Range(7,15));
+        AudioManagerSingleton.Instance.PlayRandomSongFromSoundtrackOnce("creaksound 1", gameObject);
+        yield return new WaitForSeconds(Random.Range(7,15));
+        AudioManagerSingleton.Instance.PlayRandomSongFromSoundtrackOnce("creaksound 2", gameObject);
+      }
+
     }
 }
