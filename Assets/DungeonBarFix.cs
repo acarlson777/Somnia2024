865e78cdf85c5797eb5f5edd660698b0abbb68b6
Carlson <acarlson777@monroviaschools.net>
2024-04-08 10:36:24 -0700

    yo

---
+++ Assets/DungeonBarFix.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonBarFix : MonoBehaviour { 
+    private bool barIsBehind;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
