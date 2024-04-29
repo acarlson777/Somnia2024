651a7d6b8209673058d966b5cd458360c70abb84
Daniel <dmoran55555@gmail.com>
2024-04-29 10:36:15 -0700

    hycgv j

---
+++ Assets/Scripts/Mechanics/Scenes/Dungeon & Kitchen/DungeonMusic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DungeonMusic : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(DelayedMusicPlay());
+    }
+
+    IEnumerator DelayedMusicPlay()
+    {
+        yield return new WaitForSeconds(1) ;
+        // plays drip every 4-5 seconds
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(4, 5));
+            AudioManagerSingleton.Instance.PlayRandomSongFromSoundtrackOnce("drip", gameObject);
+            print("ttestey78rysei8yghszef");
+        }
+    }
+}
