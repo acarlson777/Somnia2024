923f73d19ee5efecc175b61bb17ae884578fdd1b
Carlson <acarlson777@monroviaschools.net>
2024-05-06 10:36:06 -0700

    CAI SPHINX

---
+++ Assets/Scenes/Epoch 1 Scenes/S9 - Sphinx Cave Riddle Minigame/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour
+{
+    public static List<string> inventory;
+
+    public static bool addItem(string item)
+    {
+        if (inventory.Contains(item))
+        {
+            return false;
+        }
+
+        inventory.Add(item);
+        return true;
+    }
+
+    public static void emptyInventory()
+    {
+        inventory.Clear();
+    }
+}
---
+++ Assets/Scenes/Epoch 1 Scenes/S9 - Sphinx Cave Riddle Minigame/ItemNPC.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemNPC : CharacterDialogue
+{
+    public string item;
+
+    new private void Interact(Entity entity)
+    {
+        if (timesInteracted >= lines.Count - 3)
+        {
+            if (Inventory.addItem(item))
+            {
+                timesInteracted = lines.Count - 2;
+            }
+            timesInteracted = lines.Count - 1;
+            return;
+        }
+        base.Interact(entity);
+        timesInteracted = lines.Count - 3;
+    }
+}
