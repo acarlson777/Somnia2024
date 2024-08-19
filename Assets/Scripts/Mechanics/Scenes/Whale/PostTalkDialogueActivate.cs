b38ab3aad58b68689928b6c88fd0942f51cfe80f
Leonardo Garcia Sanchez <lgarciasanchez5450@gmail.com>
2024-08-19 09:54:52 -0700

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Mechanics/Scenes/Whale/PostTalkDialogueActivate.cs
+++ Assets/Scripts/Mechanics/Scenes/Whale/PostTalkDialogueActivate.cs
@@ -1,9 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
-using UnityEngine.WSA;
 
 public class PostTalkDialogueActivate : MonoBehaviour
 {
